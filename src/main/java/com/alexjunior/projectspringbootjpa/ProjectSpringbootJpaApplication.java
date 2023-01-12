334e1703d9086e26edf243e35f11b44466dd5566
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-12 20:02:33 -0300

    Project created

---
+++ src/main/java/com/alexjunior/projectspringbootjpa/ProjectSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.alexjunior.projectspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexjunior/projectspringbootjpa/ProjectSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexjunior.projectspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
