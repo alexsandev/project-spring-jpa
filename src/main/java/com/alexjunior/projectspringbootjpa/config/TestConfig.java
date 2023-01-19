7aebe3c7107ba28e7fc6d86b965588348911a629
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-18 23:37:48 -0300

    Payment, one-to-one association

--- src/main/java/com/alexjunior/projectspringbootjpa/config/TestConfig.java
+++ src/main/java/com/alexjunior/projectspringbootjpa/config/TestConfig.java
@@ -12,6 +12,7 @@ import org.springframework.context.annotation.Profile;
 import com.alexjunior.projectspringbootjpa.entities.Category;
 import com.alexjunior.projectspringbootjpa.entities.Order;
 import com.alexjunior.projectspringbootjpa.entities.OrderItem;
+import com.alexjunior.projectspringbootjpa.entities.Payment;
 import com.alexjunior.projectspringbootjpa.entities.Product;
 import com.alexjunior.projectspringbootjpa.entities.User;
 import com.alexjunior.projectspringbootjpa.entities.enums.OrderStatus;
@@ -80,5 +81,9 @@ public class TestConfig implements CommandLineRunner{
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        orderRepository.save(o1);
     }
 }
