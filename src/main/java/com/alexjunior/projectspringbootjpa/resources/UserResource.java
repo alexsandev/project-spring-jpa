ebb216b2fdd8ea11f897e9e65f737fd99ce05986
Alexsandro Rodrigues <alex.rsjr10@gmail.com>
2023-01-19 22:10:06 -0300

    User insert

--- src/main/java/com/alexjunior/projectspringbootjpa/resources/UserResource.java
+++ src/main/java/com/alexjunior/projectspringbootjpa/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.alexjunior.projectspringbootjpa.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.alexjunior.projectspringbootjpa.entities.User;
 import com.alexjunior.projectspringbootjpa.services.UserService;
@@ -17,15 +21,22 @@ import com.alexjunior.projectspringbootjpa.services.UserService;
 public class UserResource {
 
     @Autowired
-    private UserService services;
+    private UserService service;
 
     @GetMapping
     public ResponseEntity<List<User>> findAll(){
-        return ResponseEntity.ok().body(services.findAll());
+        return ResponseEntity.ok().body(service.findAll());
     }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<User> findById(@PathVariable Long id){
-        return ResponseEntity.ok().body(services.findById(id));
+        return ResponseEntity.ok().body(service.findById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<User> insert(@RequestBody User user){
+        service.insert(user);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 }
